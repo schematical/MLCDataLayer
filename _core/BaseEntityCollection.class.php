2873055805cace9e7e83bcdba2dcf847c5e5e74e
Matt Lea <mlea@schematical.com>
2013-09-11 12:51:05 -0500

    beefing up base entity collections

--- _core/BaseEntityCollection.class.php
+++ _core/BaseEntityCollection.class.php
@@ -1,20 +1,20 @@
 <?php
 /*
- * Manages collections of data objects
+ * Manages arrCollections of data objects
  */
 class BaseEntityCollection implements arrayaccess
 	{
 		
-		protected $collection = array();
+		protected $arrCollection = array();
 		public function getIDsAsArray(){
 			$arrReturn = array();
-			foreach($this->collection as $objEntity){
+			foreach($this->arrCollection as $objEntity){
 				$arrReturn[$objEntity->getId()] = $objEntity->getId();
 			}
 			return $arrReturn;
 		}
 		public function getRandomized(){
-			$arrColl = $this->collection;
+			$arrColl = $this->arrCollection;
 			$arrReturn = array();
 			while(count($arrColl) != 0){
 				$i = rand(0, count($arrColl) - 1);
@@ -24,29 +24,29 @@ class BaseEntityCollection implements arrayaccess
 			return $arrReturn;		
 		}
 		public function getRandom(){
-			$arrColl = $this->collection;			
+			$arrColl = $this->arrCollection;			
 			$i = rand(0, count($arrColl) - 1);
 			return $arrColl[$i];
 		}
 		
 		
 		public function getItemByIndex($index){
-			return $this->collection[$index];
+			return $this->arrCollection[$index];
 		}
 		public function addItem($item){
-			array_push($this->collection,$item);
+			array_push($this->arrCollection,$item);
 		}
 		public function getCollection(){
-			return $this->collection;
+			return $this->arrCollection;
 		}
 		public function length(){
-			return count($this->collection);
+			return count($this->arrCollection);
 		}
 		public function combine($nColl){
 			$coll = $nColl->getCollection();
 			
 			foreach($coll as $mObj){
-				array_push($this->collection,$mObj);
+				array_push($this->arrCollection,$mObj);
 			}
 			
 		}
@@ -92,7 +92,7 @@ class BaseEntityCollection implements arrayaccess
 						array_push($nColl, $mObj);
 					}
 				}
-				$this->collection = $nColl;
+				$this->arrCollection = $nColl;
 				return $bool;
 			}else{
 				return false;
@@ -104,15 +104,15 @@ class BaseEntityCollection implements arrayaccess
 			}
 		}
 		public function __construct($arrData = array()){
-			$this->collection = $arrData;
+			$this->arrCollection = $arrData;
 		}
 		public function save(){
-			foreach($this->collection as $entity){
+			foreach($this->arrCollection as $entity){
 				MLCDBDriver::save($entity);
 			}
 		}
 		public function markAllDeleted(){
-			$coll = $this->collection;
+			$coll = $this->arrCollection;
 			foreach($coll as $obj){
 				try{
 					$obj->markDeleted();
@@ -122,24 +122,24 @@ class BaseEntityCollection implements arrayaccess
 			}
 		}
 		public function removeByIndex($intIndex){
-			unset($this->collection[$intIndex]);
+			unset($this->arrCollection[$intIndex]);
 		}
         public function offsetSet($strKey, $mixValue) {
             if (is_null($strKey)) {
-                $this->collection[] = $mixValue;
+                $this->arrCollection[] = $mixValue;
             } else {
-                $this->collection[$strKey] = $mixValue;
+                $this->arrCollection[$strKey] = $mixValue;
             }
         }
         public function offsetExists($strKey) {
-            return array_key_exists($strKey, $this->collection);
+            return array_key_exists($strKey, $this->arrCollection);
         }
         public function offsetUnset($strOffset) {
-            unset($this->collection[$strOffset]);
+            unset($this->arrCollection[$strOffset]);
         }
         public function offsetGet($strOffset) {
-            if(array_key_exists($strOffset, $this->collection)){
-                 return $this->collection[$strOffset];
+            if(array_key_exists($strOffset, $this->arrCollection)){
+                 return $this->arrCollection[$strOffset];
             }else{
                 return null;
             }
